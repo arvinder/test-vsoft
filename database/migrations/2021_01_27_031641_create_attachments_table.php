0248973d495365cfe2e94fc607c4ac649dbb000d
Arvinder Singh <arvinder.arjinder@gmail.com>
2021-01-26 19:33:03 -0800

    Migrations

---
+++ database/migrations/2021_01_27_031641_create_attachments_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAttachmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('attachments', function (Blueprint $table) {
+            $table->id();
+            $table->longText('name');
+            $table->longText('content');
+            $table->bigInteger('mail_id');
+
+            $table->foreign('mail_id')->references('id')->on('mails')->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('attachments');
+    }
+}
